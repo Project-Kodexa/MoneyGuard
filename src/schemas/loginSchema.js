c3ad7f6beb787b9988f5cc07ac6021bdfacab40b
Vahide Segah Üney <vahidesegah@gmail.com>
2025-07-04 21:05:09 +0300

    Registration Page, AuthOperations oluşturuldu authSlice a eklendi

---
+++ src/schemas/loginSchema.js
@@ -0,0 +1,9 @@
+import * as yup from 'yup';
+
+
+export const loginSchema = yup.object().shape({
+    email: yup.string().email('Invalid email').required('Email is required'),
+    password: yup.string().min(7, 'Password must be at least 7 characters').max(30, 'Password must be at most 30 characters').required('Password is required'),
+});
+
+// Kişi 3 login işlemi için bu şemayı kullanacak
