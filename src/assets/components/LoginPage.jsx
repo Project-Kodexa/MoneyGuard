80a88762f860b1f75bed00730cb24420a1bc4366
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-07-05 13:34:54 +0300

    LoginForm LoginPage

--- src/assets/components/LoginPage.jsx
+++ src/assets/components/LoginPage.jsx
@@ -1,33 +1,17 @@
 import React from "react";
 import styles from "./LoginPage.module.css";
-import { FaEnvelope, FaLock } from "react-icons/fa";
+import LoginForm from "./LoginForm"; // 👈 formu çağırıyoruz
 
 const LoginPage = () => {
   return (
     <div className={styles.loginModal}>
       <div className={styles.login}>
         <div className={styles.loginLogo}>
-          <img src="../../../public/moneyGuard.svg" alt="logo" />
+          <img src="/moneyGuard.svg" alt="logo" />
           <p className={styles.loginTitle}>Money Guard</p>
         </div>
-        <div className={styles.loginInputGroup}>
-          <FaEnvelope className={styles.loginIcon} />
-          <input
-            type="email"
-            placeholder="E-mail"
-            className={styles.loginInput}
-          />
-        </div>
-        <div className={styles.loginInputGroup}>
-          <FaLock className={styles.loginIcon} />
-          <input
-            type="password"
-            placeholder="Password"
-            className={styles.loginInput}
-          />
-        </div>
-        <button className={styles.loginBtn}>LOG IN</button>
-        <button className={styles.registerBtn}>REGİSTER</button>
+
+        <LoginForm />
       </div>
     </div>
   );
