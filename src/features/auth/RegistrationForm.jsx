7ee727d1f003df93144b5e68d2d94fc3f169c4a5
Vahide Segah Üney <vahidesegah@gmail.com>
2025-07-05 09:38:02 +0300

    Registration Form bileşeni yazıldı

---
+++ src/features/auth/RegistrationForm.jsx
@@ -0,0 +1,32 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { registerSchema } from '../../schemas/registerSchema';
+
+
+function RegistrationForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('username')} placeholder="Kullanıcı Adı" />
+      {errors.username && <p>{errors.username.message}</p>}
+
+      <input {...register('email')} placeholder="E-posta" />
+      {errors.email && <p>{errors.email.message}</p>}
+
+      <input type="password" {...register('password')} placeholder="Parola" />
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <input type="password" {...register('passwordConfirm')} placeholder="Parola Tekrar" />
+      {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
+
+      <button type="submit">Kaydol</button>
+    </form>
+  );
+}
+
+export default RegistrationForm;
