7ff9fdf9193e9e96e21b9e3a36eff896a046e3d8
Vahide Segah Üney <vahidesegah@gmail.com>
2025-07-05 09:38:02 +0300

    Registration Form bileşeni yazıldı

--- src/features/auth/RegistrationForm.jsx
+++ src/features/auth/RegistrationForm.jsx
@@ -1,82 +1,30 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { registerSchema } from '../../schemas/registerSchema';
-import styles from './RegistrationForm.module.css';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { setCredentials } from '../../features/auth/authSlice'; 
+
 
 function RegistrationForm() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm({
+  const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: yupResolver(registerSchema),
   });
 
-  const onSubmit = (data) => {
-    const { ...userData } = data;
-
-    // Backend olmadığı için mockToken kullandım
-    const userToSave = {
-      token: `mock_token_${Date.now()}`,
-      user: {
-        name: userData.username,
-        email: userData.email,
-        balance: 0, // Başlangıçta 0 bakiye
-      },
-    };
-    // Redux store'a kullanıcı bilgilerini kaydet
-    dispatch(setCredentials(userToSave));
-
-    console.log("Kayıt başarılı ve Redux güncellendi!", userToSave);
-    alert('Kayıt başarılı! Şimdi giriş yapabilirsiniz.');
-
-    // Kayıt başarılıysa kullanıcıyı Giriş sayfasına yönlendir
-    navigate('/login');
-    reset();
-  };
+  const onSubmit = (data) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-      <input
-        className={styles.input}
-        {...register('username')}
-        placeholder="Username"
-      />
-      {errors.username && <p className={styles.errorMessage}>{errors.username.message}</p>}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('username')} placeholder="Kullanıcı Adı" />
+      {errors.username && <p>{errors.username.message}</p>}
 
-      <input
-        className={styles.input}
-        {...register('email')}
-        placeholder="E-mail"
-      />
-      {errors.email && <p className={styles.errorMessage}>{errors.email.message}</p>}
+      <input {...register('email')} placeholder="E-posta" />
+      {errors.email && <p>{errors.email.message}</p>}
 
-      <input
-        type="password"
-        className={styles.input}
-        {...register('password')}
-        placeholder="Password"
-      />
-      {errors.password && <p className={styles.errorMessage}>{errors.password.message}</p>}
+      <input type="password" {...register('password')} placeholder="Parola" />
+      {errors.password && <p>{errors.password.message}</p>}
 
-      <input
-        type="password"
-        className={styles.input}
-        {...register('confirmPassword')}
-        placeholder="Confirm Password"
-      />
-      {errors.confirmPassword && <p className={styles.errorMessage}>{errors.confirmPassword.message}</p>}
+      <input type="password" {...register('passwordConfirm')} placeholder="Parola Tekrar" />
+      {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
 
-      <button type="submit" className={styles.button_reg}>
-        Register
-      </button>
+      <button type="submit">Kaydol</button>
     </form>
   );
 }
