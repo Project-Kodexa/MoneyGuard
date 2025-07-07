2b548f484bc324b70ad0a2a9dec846ccff697939
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-07-07 14:09:31 +0300

    component dosyasını silip ortak olana taşıdım

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./LoginPage.module.css";
-import LoginForm from "./LoginForm"; 
+import LoginForm from "../Login/LoginForm"; 
 
 const LoginPage = () => {
   return (
