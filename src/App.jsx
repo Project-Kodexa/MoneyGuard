0529d996930b1c934653c70cbde5f307290da25b
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-07-07 14:21:46 +0300

    rootReducer global olanı ekledim

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./routes/PrivateRoute";
 import PublicRoute from "./routes/PublicRoute";
 import LoginPage from "./components/Login/LoginPage";
+import { useSelector } from "react-redux";
 // import LoginPage from "./features/auth/LoginPage";
 // import RegistrationPage from "./features/auth/RegistrationPage";
 // import DashboardPage from "./pages/DashboardPage";
 
 function App() {
- habibe
-const isLoading = useSelector((state) => state.global.isLoading);
+  const isLoading = useSelector((state) => state.global.isLoading);
 
-
- main
   return (
-     <div>
-
+    <div>
       {isLoading && <Loader />}
-    <BrowserRouter>
-      <Routes>
-        <Route element={<PublicRoute />}>
-          <Route
-            path="/register"
-            element={<div>Registration Page Placeholder</div>}
-          />
-          <Route path="/login" element={<LoginPage />} />
-        </Route>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route
+              path="/register"
+              element={<div>Registration Page Placeholder</div>}
+            />
+            <Route path="/login" element={<LoginPage />} />
+          </Route>
 
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<div>Dash Board Page Placeholder</div>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<div>Dash Board Page Placeholder</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
