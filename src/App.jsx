0a23fe220f3bc15121de912993d099de9d9d5da7
Habibe Çınar <146085701+habibecinar@users.noreply.github.com>
2025-07-08 21:55:43 +0300

    add currentyTab

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,17 @@ import Loader from "./components/Loader/Loader.jsx";
 import LoginPage from "../src/components/Login/LoginPage.jsx";
 import RegistrationPage from "./features/auth/RegistrationPage.jsx";
 import DashboardPage from "../src/pages/Dashboard.jsx";
+import { useDispatch } from "react-redux";
+import { setLoading } from "./redux/globalSlice"; // doğru dosya yoluna göre ayarla
+
 
 function App() {
   const isLoading = useSelector((state) => state.global.isLoading);
-
+const dispatch = useDispatch();
   return (
     <div>
+    <button onClick={() => dispatch(setLoading(true))}>Yüklemeyi Başlat</button>
+      <button onClick={() => dispatch(setLoading(false))}>Yüklemeyi Durdur</button>
       {isLoading && <Loader />}
       <BrowserRouter>
         <Routes>
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,10 +2,6 @@ import { combineReducers } from "redux";
 import authReducer from "../features/auth/authSlice";
 import transactionsReducer from "../redux/transactionsSlice";
 import globalReducer from "../redux/globalSlice";
-<<<<<<< HEAD
-=======
-
->>>>>>> 93cb302c28852ebff140e6e0a25914f4c2b9d33b
 const rootReducer = combineReducers({
   auth: authReducer,
   transactions: transactionsReducer,
