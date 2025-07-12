901487ab83ccc4c33456014af6920cbfec9cc35e
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-07-12 23:41:21 +0300

    added registration bckGround

--- src/features/auth/RegistrationPage.jsx
+++ src/features/auth/RegistrationPage.jsx
@@ -5,6 +5,7 @@ import styles from "./RegistrationPage.module.css";
 function RegistrationPage() {
   return (
     <div className={styles.pageContainer}>
+      <img src="/moneyGuard.svg" alt="logo" />
       <h1 className={styles.title}>Money Guard</h1>
       <RegistrationForm />
     </div>
