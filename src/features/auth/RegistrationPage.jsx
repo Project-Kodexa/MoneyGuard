7a702b38b00cd04af521e6e309e1c69e5fab4e89
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-07-12 23:38:15 +0300

    register added css

--- src/features/auth/RegistrationPage.jsx
+++ src/features/auth/RegistrationPage.jsx
@@ -1,20 +1,12 @@
-import React from 'react';
-
-import { Link } from 'react-router-dom';
-import RegistrationForm from '../auth/RegistrationForm.jsx';
-import styles from './RegistrationPage.module.css'; 
+import React from "react";
+import RegistrationForm from "../auth/RegistrationForm.jsx";
+import styles from "./RegistrationPage.module.css";
 
 function RegistrationPage() {
   return (
     <div className={styles.pageContainer}>
-      <h1 className={styles.title}>Register Account</h1>
+      <h1 className={styles.title}>Money Guard</h1>
       <RegistrationForm />
-      <p className={styles.loginText}>
-        Already have an account?
-        <Link to="/login" className={styles.loginLink}>
-          Login
-        </Link>
-      </p>
     </div>
   );
 }
